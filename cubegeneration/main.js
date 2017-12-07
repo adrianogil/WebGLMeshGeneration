7e2dc58fe92a96d3caff4363d14c78dfb635b445
Adriano Gil <adrianogil.san@gmail.com>
2017-12-07 12:45:51 -0400

    Generate a Dodecahedron

--- cubegeneration/main.js
+++ cubegeneration/main.js
@@ -20,10 +20,15 @@ function main()
     MeshBuilder.createCube(cube, p, v1, v2, v3);
     scene.addObject(cube);
 
+    var dode = new GLMeshObject("dodecahedron");
+    Dodecahedron.create(dode);
+    scene.addObject(dode);
+
     function update(now)
     {
         quad.transform.rotate(new Vector3(0.0, 0.01, 0.01));
         cube.transform.rotate(new Vector3(0.0, -0.01, -0.01));
+        dode.transform.rotate(new Vector3(0.01, 0.01, -0.01));
 
         requestAnimationFrame(update);
     }
